6c69d548c10d56c96b41aba35fe81009e06ee96b
Kevin Goode <kevin.goode@hpe.com>
2020-01-16 13:15:55 +0000

    Updated help in build script

--- example/build.sh
+++ example/build.sh
@@ -4,7 +4,7 @@ docker run -v ${PWD}/src:/cpp-dev grpc_cpp:latest make
 docker run -v ${PWD}/src:/cpp-dev grpc_cpp:latest cp /usr/local/lib/libgrpc++.so.1 ./
 docker run -v ${PWD}/src:/cpp-dev grpc_cpp:latest cp /usr/local/lib/libgrpc.so.7 ./
 docker run -v ${PWD}/src:/cpp-dev grpc_cpp:latest cp /usr/local/lib/libgrpc++_reflection.so.1 ./
-echo "To run code in one shell execute : src> export LD_LIBRARY_PATH=`pwd`/src; ./hello_server"
+echo "To run code, you need two shells.  In one shell execute : src> export LD_LIBRARY_PATH=`pwd`/src; ./hello_server"
 echo "In a second shell execute : src> export LD_LIBRARY_PATH=`pwd`/src; ./hello_client"
 
 
